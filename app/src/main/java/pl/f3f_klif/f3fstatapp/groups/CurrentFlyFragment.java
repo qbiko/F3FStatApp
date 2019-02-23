dab19581a152e9b308d5c326fdaafefe2b59ede0
Sebastian Sarnecki <ssarnecki34@gmail.com>
2019-02-23 13:12:52 +0100

    init implementation of groups

---
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/groups/CurrentFlyFragment.java
@@ -0,0 +1,9 @@
+package pl.f3f_klif.f3fstatapp.groups;
+
+import android.support.v4.app.Fragment;
+
+public class CurrentFlyFragment extends Fragment {
+    public static CurrentFlyFragment newInstance() {
+        return new CurrentFlyFragment();
+    }
+}
---
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/groups/RoundFragment.java
@@ -0,0 +1,10 @@
+package pl.f3f_klif.f3fstatapp.groups;
+
+import android.support.v4.app.Fragment;
+
+public class RoundFragment extends Fragment {
+
+    public static RoundFragment newInstance() {
+        return new RoundFragment();
+    }
+}
