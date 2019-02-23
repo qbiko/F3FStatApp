15c9cc60cc034f11aff7c6e6c13e5688c7cd1a87
Sebastian Sarnecki <ssarnecki34@gmail.com>
2019-02-23 18:55:46 +0100

    data in group from vault

--- app/src/main/java/pl/f3f_klif/f3fstatapp/groups/services/GroupCreator.java
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/groups/services/GroupCreator.java
@@ -6,24 +6,32 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import pl.f3f_klif.f3fstatapp.R;
 import pl.f3f_klif.f3fstatapp.groups.infrastructure.ItemAdapter;
 import pl.f3f_klif.f3fstatapp.groups.services.models.Group;
+import pl.f3f_klif.f3fstatapp.utils.Pilot;
 
 public class GroupCreator {
 
-    public static Group Create(Context context, String groupName){
+    public static Group Create(Context context, String groupName, List<Pilot> pilots){
         final ArrayList<Pair<Long, String>> mItemArray = new ArrayList<>();
-        int addItems = 12;
-        for (long i = 0; i < addItems; i++) {
-            mItemArray.add(new Pair<>(i, "Zawodnik " + i));
+        long index = 0;
+        for (Pilot pilot: pilots) {
+            mItemArray
+                    .add(new Pair<>(
+                            index,
+                            String.format("%s %s", pilot.getFirstName(), pilot.getLastName())
+                            )
+                    );
+            index++;
         }
 
         ItemAdapter listAdapter = new ItemAdapter(mItemArray, R.layout.group_pilot_item, R.id.item_layout, true);
         final View header = View.inflate(context, R.layout.group_header, null);
         ((TextView) header.findViewById(R.id.text)).setText(groupName);
-        ((TextView) header.findViewById(R.id.item_count)).setText("" + addItems);
+        ((TextView) header.findViewById(R.id.item_count)).setText("" + pilots.size());
 
         return new Group(listAdapter, header, false);
     }
