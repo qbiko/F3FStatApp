51b37aecb24077265cf6c4da59dfa300d4cb3cd4
Jakub Chodorowski <chodorowski.jakub@gmail.com>
2019-03-17 21:17:19 +0100

    rozwiazanie konfliktow

--- app/src/main/java/pl/f3f_klif/f3fstatapp/groups/services/points/PointCounter.java
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/groups/services/points/PointCounter.java
@@ -7,7 +7,6 @@ import com.annimon.stream.Stream;
 
 import org.apache.commons.math3.util.Precision;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
