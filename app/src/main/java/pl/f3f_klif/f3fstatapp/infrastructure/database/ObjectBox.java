a753ed5f0345667b73303b96b8dc406af326304d
Jakub Chodorowski <chodorowski.jakub@gmail.com>
2019-03-19 22:29:39 +0100

    singleton in objectbox init

--- app/src/main/java/pl/f3f_klif/f3fstatapp/infrastructure/database/ObjectBox.java
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/infrastructure/database/ObjectBox.java
@@ -10,9 +10,11 @@ public class ObjectBox {
     private static BoxStore boxStore;
 
     public static void init(Context context) {
-        boxStore = MyObjectBox.builder()
-                .androidContext(context.getApplicationContext())
-                .build();
+        if(boxStore == null) {
+            boxStore = MyObjectBox.builder()
+                    .androidContext(context.getApplicationContext())
+                    .build();
+        }
     }
 
     public static BoxStore get() {
