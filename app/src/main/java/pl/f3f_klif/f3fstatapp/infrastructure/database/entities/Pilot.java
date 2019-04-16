a7a5cd0deb302dc8457ba7fbf2bd64e93e2ee073
Sebastian Sarnecki <ssarnecki34@gmail.com>
2019-04-16 19:10:45 +0200

    order fixed

--- app/src/main/java/pl/f3f_klif/f3fstatapp/infrastructure/database/entities/Pilot.java
+++ app/src/main/java/pl/f3f_klif/f3fstatapp/infrastructure/database/entities/Pilot.java
@@ -3,6 +3,8 @@ package pl.f3f_klif.f3fstatapp.infrastructure.database.entities;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import io.objectbox.Box;
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
@@ -23,7 +25,6 @@ public class Pilot  implements Parcelable {
     private String fai;
     private String faiLicense;
     private String teamName;
-
     ToMany<Result> results;
 
     public Pilot(){ }
@@ -62,6 +63,9 @@ public class Pilot  implements Parcelable {
 
     public long getF3fId() {
         return f3fId;
+    }
+    public long getId() {
+        return id;
     }
 
     public int getStartNumber() {
